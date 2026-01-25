c3c19e365990b819a7719dbe78050819cd9c98d0
juanpabloruiz <pabloruiz1980@gmail.com>
2026-01-24 21:58:20 -0300

    I add Bootstrap

--- roles.php
+++ roles.php
@@ -1,61 +1,94 @@
 <?php require_once 'conn.php' ?>
+<!DOCTYPE html>
+<html lang="es">
 
-<?php
+<head>
 
-// Editar registro
-if (isset($_GET['edit'])):
-    $id = (int) $_GET['edit'];
-    $stmt = $conn->prepare("SELECT * FROM roles WHERE id = ?");
-    $stmt->bind_param('i', $id);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    if (!$row):
-        header('Location: roles.php');
-        exit;
-    endif;
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfiles</title>
 
-?>
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    <form method="POST">
-        <input type="hidden" name="id" value="<?= $row['id'] ?>">
-        <input type="text" name="title" value="<?= $row['title'] ?>">
-        <input type="submit" name="update" value="Actualizar">
-    </form>
+</head>
 
-<?php else: ?>
+<body>
 
-    <form method="POST">
-        <input type="text" name="title" placeholder="Perfil">
-        <input type="submit" name="insert" value="Insertar">
-    </form>
+    <main class="container-fluid my-3">
 
-<?php endif ?>
+        <?php
 
-<table>
+        // Editar registro
+        if (isset($_GET['edit'])):
+            $id = (int) $_GET['edit'];
+            $stmt = $conn->prepare("SELECT * FROM roles WHERE id = ?");
+            $stmt->bind_param('i', $id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            if (!$row):
+                header('Location: roles.php');
+                exit;
+            endif;
 
-    <thead>
-        <tr>
-            <th>TÍTULO</th>
-            <th>CREADO</th>
-        </tr>
-    </thead>
+        ?>
 
-    <?php
-    $query = $conn->query("SELECT * FROM roles");
-    while ($row = $query->fetch_assoc()):
-    ?>
+            <!-- Form Insert -->
+            <form method="POST">
+                <input type="hidden" name="id" value="<?= $row['id'] ?>">
+                <input type="text" name="title" class="form-control" value="<?= $row['title'] ?>">
+                <input type="submit" name="update" value="Actualizar" class="btn btn-primary">
+            </form>
 
-        <tbody>
-            <tr>
-                <td onclick="window.location='?edit=<?= $row['id'] ?>'"><?= htmlspecialchars($row['title']) ?></td>
-                <td onclick="window.location='?edit=<?= $row['id'] ?>'"><?= date('d/m/Y ⏰ H:i', strtotime($row['created_at'])) ?></td>
-            </tr>
-        </tbody>
+        <?php else: ?>
 
-    <?php endwhile ?>
+            <!-- Form Update -->
+            <form method="POST">
+                <input type="text" name="title" class="form-control" placeholder="Perfil">
+                <input type="submit" name="insert" value="Insertar" class="btn btn-primary">
+            </form>
 
-</table>
+        <?php endif ?>
+
+        <!-- Table to display records -->
+        <div class="table-responsive">
+            
+            <table class="table table-hover">
+
+                <thead>
+                    <tr>
+                        <th>TÍTULO</th>
+                        <th>CREADO</th>
+                    </tr>
+                </thead>
+
+                <?php
+                $query = $conn->query("SELECT * FROM roles");
+                while ($row = $query->fetch_assoc()):
+                ?>
+
+                    <tbody>
+                        <tr>
+                            <td onclick="window.location='?edit=<?= $row['id'] ?>'"><?= htmlspecialchars($row['title']) ?></td>
+                            <td onclick="window.location='?edit=<?= $row['id'] ?>'"><?= date('d/m/Y ⏰ H:i', strtotime($row['created_at'])) ?></td>
+                        </tr>
+                    </tbody>
+
+                <?php endwhile ?>
+
+            </table>
+
+        </div>
+
+    </main>
+
+    <!-- Scripts -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
 
 <?php
 
