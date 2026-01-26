144b8b653ce12dc9cd09749bce449d6e56d2d5b2
juanpabloruiz <pabloruiz1980@gmail.com>
2026-01-26 15:22:52 -0300

    Añadir barra de navegación a la página de roles

--- roles.php
+++ roles.php
@@ -16,6 +16,25 @@
 
 <body data-bs-theme="dark">
 
+    <!-- Nav -->
+    <nav class="navbar bg-dark navbar-expand-lg border-bottom" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Xegur</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="roles.php">Perfiles</a>
+                    </li>
+            </div>
+        </div>
+    </nav>
+
     <main class="container my-3">
 
         <?php
